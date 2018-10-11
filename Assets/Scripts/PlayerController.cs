9c56c1a18da2a865897c41db6906959f8550261a
Robert Geil <robert.geil999@gmail.com>
2018-10-10 22:23:05 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+	[Range(50, 200)]
+	public float speed = 100;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		rb.AddForce(new Vector3(x,0,z) * Time.deltaTime * speed);
+	}
+}
